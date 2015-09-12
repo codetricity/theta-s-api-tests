8ddf234d5e9ae26665d2880f80f2a428b74b103e
Craig Oda <craig@oppkey.com>
2015-09-11 20:18:31 -0700

    added graphics to desktop app

--- thetaPythonDesktop.py
+++ thetaPythonDesktop.py
@@ -10,12 +10,20 @@ DARK = (64, 64, 64)
 
 
 pygame.init()
-SCREEN = pygame.display.set_mode((800, 600))
+SCREENSIZE = (800, 600)
+SCREEN = pygame.display.set_mode(SCREENSIZE)
 
-button = pygame.Rect(100, 100, 140, 80)
+button = pygame.Rect(100, 120, 140, 80)
 font = pygame.font.Font("fnt/Lato-Thin.ttf", 20)
 button_text = font.render("Take Picture", True, DARK)
 text_box = (button.left + 15, button.top + 23)
+
+theta = pygame.image.load("img/ricoh-theta-s.png")
+theta_rect = theta.get_rect()
+theta_rect.right = SCREENSIZE[0] - 50
+theta_rect.top = 60
+
+developers_logo = pygame.image.load("img/theta_developers.png")
 
 while True:
     for event in pygame.event.get():
@@ -32,5 +40,7 @@ while True:
     pygame.draw.rect(SCREEN,GRAY, button)
     pygame.draw.rect(SCREEN, DARK, button, 1)
     SCREEN.blit(button_text, text_box)
+    SCREEN.blit(theta, theta_rect)
+    SCREEN.blit(developers_logo, (10, 10))
 
     pygame.display.update()
